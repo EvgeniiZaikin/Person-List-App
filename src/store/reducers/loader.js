c6c888cc67079eadc5da551b20d762cba9db0eb1
en_zaikin <en_zaikin@bingo-boom.com>
2020-09-17 15:00:35 +0300

    Added actions, action-creators with redux.

--- src/store/reducers/loader.js
+++ src/store/reducers/loader.js
@@ -1,6 +1,8 @@
+import { TOGGLE_LOADER } from '../actions/actions';
+
 export default (state = { loader: false }, { type, payload }) => {
     switch(type) {
-        case 'TOGGLE_LOADER':
+        case TOGGLE_LOADER:
             return { ...state, loader: payload };
         default:
             return { ...state, loader: false };
