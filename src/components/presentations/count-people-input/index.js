b79bbc5f1af4de5aebdc49e554d3f478a16b1514
en_zaikin <en_zaikin@bingo-boom.com>
2020-09-21 16:36:03 +0300

    Set check only digit for count-people-input

--- src/components/presentations/count-people-input/index.js
+++ src/components/presentations/count-people-input/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './style.css';
 
+const _checkDigit = event => {
+    const keyCode = event.keyCode || event.which;
+    if (keyCode <= 48 || keyCode >= 57) {
+        event.preventDefault();
+    }
+};
+
 const countPeopleInput = ({ change, defaultValue }) => (
     <div className='set-count-people'>
         <label>Введите количество человек: &nbsp;</label>
         <input
             defaultValue={ defaultValue }
-            onChange={ event => change(event.target.value) } />
+            onChange={ event => change(event.target.value) }
+            onKeyPress={ event => _checkDigit(event) } />
     </div>
 );
 
@@ -15,5 +23,5 @@ export default countPeopleInput;
 
 countPeopleInput.propTypes = {
     change: PropTypes.func.isRequired,
-    defaultValue: PropTypes.number.isRequired,
+    defaultValue: PropTypes.string.isRequired,
 };
